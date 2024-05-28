890c30ce813c3a9ec45e8de369165780cc7685ce
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2024-05-29 04:12:15 +0700

    feat: added filter

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,43 +1,108 @@
-import { PROJECTS } from "@/data/Projects.ts";
+import Filter from "@/components/Filter";
+import { FILTERLOCATION, FILTERTYPE } from "@/data/Filter.ts";
+import type { CollectionEntry } from "astro:content";
 import ProjectCard from "./ProjectCard.tsx";
 
-// import { React, useState, useEffect } from "react";
+import { useMemo, useState } from "react";
 
 interface Props {
   slice?: boolean;
-  withTitle?: boolean;
-  withButton?: boolean;
+  homePage?: boolean;
+  projectsPage?: boolean;
+  slugItems: CollectionEntry<"projects">[];
 }
 
-const Projects = ({ slice, withTitle = true, withButton = true }: Props) => {
+const Projects = ({
+  slice,
+  homePage = true,
+  projectsPage = false,
+  slugItems,
+}: Props) => {
+  const [filterTypes, setFilterTypes] = useState<string>("All");
+  const [filterLocation, setFilterLocation] = useState<string>("All");
+  const filteredItems = useMemo(() => {
+    return slugItems.filter((item) => {
+      if (filterTypes === "All" && filterLocation === "All") {
+        return true;
+      } else if (filterTypes === "ทาสี" && filterLocation === "All") {
+        return !item.data.tags.includes("งานออกแบบ");
+      } else if (filterTypes === "ทาสี") {
+        return (
+          !item.data.tags.includes("งานออกแบบ") &&
+          item.data.tags.includes(filterLocation)
+        );
+      } else if (filterLocation === "All") {
+        return item.data.tags.includes(filterTypes);
+      } else if (filterTypes === "All") {
+        return item.data.tags.includes(filterLocation);
+      } else {
+        return (
+          item.data.tags.includes(filterTypes) &&
+          item.data.tags.includes(filterLocation)
+        );
+      }
+    });
+  }, [filterTypes, filterLocation, slugItems]);
+
+  const changeType = (type: string) => {
+    setFilterTypes(type);
+  };
+
+  const changeLocation = (location: string) => {
+    setFilterLocation(location);
+  };
+
   return (
-    <section className="mx-auto max-w-7xl p-6">
-      {withTitle && (
-        <h1 className="mb-12 text-center font-Inter text-[40px] font-semibold uppercase text-primary-800">
+    <section className="mx-auto max-w-7xl px-6">
+      {projectsPage && (
+        <div className="px-6 pb-6">
+          <h1 className="pb-8 font-Inter text-[40px] font-bold text-primary-800">
+            Projects
+          </h1>
+          <div className="flex flex-row space-x-8">
+            <Filter key="filterTypes" data={FILTERTYPE} setState={changeType} />
+            <Filter
+              key="filterLocation"
+              data={FILTERLOCATION}
+              setState={changeLocation}
+            />
+          </div>
+        </div>
+      )}
+      {homePage && (
+        <h1 className="mb-12 mt-4 text-center font-Inter text-[40px] font-semibold uppercase text-primary-800">
           our Projects
         </h1>
       )}
       <div className="grid grid-cols-3 gap-4">
-        {slice && PROJECTS != undefined
-          ? PROJECTS?.slice(0, 9).map((company) => (
-              <ProjectCard
-                key={company.name}
-                title={company.name}
-                image={company.image}
-              />
-            ))
-          : PROJECTS?.map((company) => (
-              <ProjectCard
-                key={company.name}
-                title={company.name}
-                image={company.image}
-              />
-            ))}
+        {slice
+          ? slugItems?.slice(0, 9).map((item, index) => {
+              return (
+                <ProjectCard
+                  key={index}
+                  slugRef={item.slug}
+                  title={item.data.title}
+                  thumbnail={item.data.heroImage}
+                />
+              );
+            })
+          : filteredItems?.map((item, index) => {
+              return (
+                <ProjectCard
+                  key={index}
+                  slugRef={item.slug}
+                  title={item.data.title}
+                  thumbnail={item.data.heroImage}
+                />
+              );
+            })}
       </div>
-      {withButton && (
-        <button className="mx-auto mt-8 flex rounded-full border border-primary-800 bg-white px-[13px] py-[10px] lowercase transition duration-300 ease-in-out hover:bg-primary-800 hover:text-white">
-          <a href="/project">See More</a>
-        </button>
+      {slice && (
+        <a href="/projects">
+          <button className="mx-auto mt-8 flex rounded-full border border-primary-800 bg-white px-[13px] py-[10px] lowercase transition duration-300 ease-in-out hover:bg-primary-800 hover:text-white">
+            See More
+          </button>
+        </a>
       )}
     </section>
   );
