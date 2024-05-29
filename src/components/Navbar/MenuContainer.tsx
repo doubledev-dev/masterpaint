86228e664fc5cd51bf30ef5d95d814e78049e7d4
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-05-29 20:36:05 +0700

    feat: menu bar in nav bar

---
+++ src/components/Navbar/MenuContainer.tsx
@@ -0,0 +1,38 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import { Menu } from "lucide-react";
+
+export default function MenuBar() {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>
+          <Menu />
+        </MenubarTrigger>
+        <MenubarContent className="text-primary-800">
+          <a href="/">
+            <MenubarItem>Home</MenubarItem>
+          </a>
+          <a href="/services">
+            <MenubarItem>Services</MenubarItem>
+          </a>
+          <a href="/projects">
+            <MenubarItem>Projects</MenubarItem>
+          </a>
+          <a href="/about">
+            <MenubarItem>About Us</MenubarItem>
+          </a>
+          <a href="/contact">
+            <MenubarItem>Contact</MenubarItem>
+          </a>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
