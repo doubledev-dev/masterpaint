3746bed7c235a31a569df6c70f29445f9cd7b120
boomchanotai <boom9387@hotmail.com>
2024-05-30 19:07:09 +0700

    feat: initialize translate

--- src/i18n/translate_en.ts
+++ src/i18n/translate_en.ts
@@ -1,3 +1,6 @@
 export const en = {
+  "hero.title": "Master Paint",
+  "hero.description":
+    "We offer painting services provided by a team of specialized and experienced professionals, who are experts specifically in painting work.",
   title: "Hello World",
 };
