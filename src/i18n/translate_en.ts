c7bba7a3914f794db01d47d61decb587226e0eb4
boomchanotai <boom9387@hotmail.com>
2024-05-30 20:24:19 +0700

    feat: add company

--- src/i18n/translate_en.ts
+++ src/i18n/translate_en.ts
@@ -8,6 +8,7 @@ export const en = {
   "hero.title": "Master Paint",
   "hero.description":
     "We offer painting services provided by a team of specialized and experienced professionals, who are experts specifically in painting work.",
+  "hero.contact": "Contact Us",
 
   "services.consult.title": "Painting consultations and estimation",
   "services.consult.description": "We plan according to your budget.",
@@ -23,6 +24,15 @@ export const en = {
   "services.design.title": "Design and Remodeling",
   "services.design.description":
     "We tailor to meet customers' needs at reasonable prices.",
+
+  "company.p1":
+    "NorthTech Co., Ltd. was established on August 18, 2005, with the objective of providing various painting services. Our team comprises experienced and specialized painters.",
+  "company.p2":
+    "We are dedicated to ensuring customer satisfaction by offering quality products and services at reasonable prices. Therefore, when you choose us, you're not just receiving a service but initiating a long-lasting relationship built on trust and excellence.",
+  "company.more": "more about us",
+  "company.offer1": "Flexible scheduling options",
+  "company.offer2": "Free consultation",
+  "company.offer3": "Maintain a clean work area",
 
   title: "Hello World",
 };
