da3cd6e21b28fde86c20034373ded9b4885d9c09
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2024-05-31 00:00:28 +0700

    feat: en

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,14 @@ import { defineCollection, z } from "astro:content";
 const projectCollection = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string().optional(),
-    location: z.string().optional(),
+    titleTH: z.string(),
+    titleEN: z.string(),
+    locationTH: z.string(),
+    locationEN: z.string(),
     tags: z.array(z.string()),
     heroImage: z.string(),
-    designDetails: z.string().optional(),
+    designDetailsTH: z.string().optional(),
+    designDetailsEN: z.string().optional(),
     pubDate: z.coerce.date().optional(),
     parity: z.number(),
   }),
