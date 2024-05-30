524a3ca14852ac21fe45c91eafe78a11b88e5241
boomchanotai <boom9387@hotmail.com>
2024-05-30 19:29:07 +0700

    feat: navbar translation

--- src/i18n/translate_en.ts
+++ src/i18n/translate_en.ts
@@ -1,6 +1,13 @@
 export const en = {
+  "navbar.home": "Home",
+  "navbar.services": "Services",
+  "navbar.projects": "Projects",
+  "navbar.about": "About Us",
+  "navbar.contact": "Contact Us",
+
   "hero.title": "Master Paint",
   "hero.description":
     "We offer painting services provided by a team of specialized and experienced professionals, who are experts specifically in painting work.",
+
   title: "Hello World",
 };
--- src/i18n/translate_th.ts
+++ src/i18n/translate_th.ts
@@ -1,4 +1,10 @@
 export const th = {
+  "navbar.home": "หน้าแรก",
+  "navbar.services": "บริการ",
+  "navbar.projects": "โปรเจค",
+  "navbar.about": "เกี่ยวกับเรา",
+  "navbar.contact": "ติดต่อเรา",
+
   "hero.title": "Master Paint",
   "hero.description":
     "รับบริการงานทาสีโดยทีมช่าง ผู้ชำนาญงานเฉพาะด้านและมีประสบการณ์ โดยตรงเชี่ยวชาญด้านงานสีโดยเฉพาะ",
