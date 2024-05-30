d938823dba237e72e09572ba99523b51ba841a40
boomchanotai <boom9387@hotmail.com>
2024-05-30 19:45:42 +0700

    feat: service

--- src/i18n/translate_en.ts
+++ src/i18n/translate_en.ts
@@ -9,5 +9,20 @@ export const en = {
   "hero.description":
     "We offer painting services provided by a team of specialized and experienced professionals, who are experts specifically in painting work.",
 
+  "services.consult.title": "Painting consultations and estimation",
+  "services.consult.description": "We plan according to your budget.",
+
+  "services.painting.title": "Painting",
+  "services.painting.description":
+    "We paint buildings, factories, residences, machinery, etc.",
+
+  "services.renovation.title": "Repair and Renovation",
+  "services.renovation.description":
+    "To extend the lifespan and enhance the modern appearance.",
+
+  "services.design.title": "Design and Remodeling",
+  "services.design.description":
+    "We tailor to meet customers' needs at reasonable prices.",
+
   title: "Hello World",
 };
--- src/i18n/translate_th.ts
+++ src/i18n/translate_th.ts
@@ -9,5 +9,19 @@ export const th = {
   "hero.description":
     "รับบริการงานทาสีโดยทีมช่าง ผู้ชำนาญงานเฉพาะด้านและมีประสบการณ์ โดยตรงเชี่ยวชาญด้านงานสีโดยเฉพาะ",
 
+  "services.consult.title": "ให้คำปรึกษาและประเมินราคางานสีทุกชนิด",
+  "services.consult.description": "วางแผนตามงบประมาณที่ต้องการ",
+
+  "services.painting.title": "ทาสี",
+  "services.painting.description": "ทาสีอาคาร โรงงาน ที่พักอาศัย เครื่องจักร ฯ",
+
+  "services.renovation.title": "รับซ่อมแซมและปรับปรุง",
+  "services.renovation.description":
+    "เพื่อยืดอายุการใช้งานและให้ดูดีทันสมัยมากขึ้น",
+
+  "services.design.title": "รับออกแบบและต่อเติม",
+  "services.design.description":
+    "ให้ตรงกับความต้องการของลูกค้าในราคาที่เหมาะสม",
+
   title: "สวัสดีชาวโลก",
 };
