4b8a994de7dc6861bce3dd340aeb29ab9b3d6022
ChanatpakornD <csirintronsopon8@gmail.com>
2024-05-31 16:14:47 +0700

    feat: improve SEO

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -32,6 +32,7 @@ const MenubarTrigger = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <MenubarPrimitive.Trigger
     ref={ref}
+    aria-label="Menu"
     className={cn(
       "flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
       className
