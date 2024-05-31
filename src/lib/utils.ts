022f953bb8ba2e13ac516658ad5389c486be9aea
boomchanotai <boom9387@hotmail.com>
2024-05-31 14:14:36 +0700

    fix: type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-type TagsMapping = Tags & "painting";
+type TagsMapping = Tags | "painting";
 
 export function mappingTags(tags: TagsMapping, lang: lang = "th") {
   const t = useTranslations(lang);
