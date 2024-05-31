6a0e3d68ed509bd906e045f538191595a243ec54
boomchanotai <boom9387@hotmail.com>
2024-05-31 12:37:13 +0700

    fix: lint

--- src/components/Navbar/LanguageSwitcher.tsx
+++ src/components/Navbar/LanguageSwitcher.tsx
@@ -17,15 +17,15 @@ const LanguageSwitcher = ({
       <label className="relative inline-flex cursor-pointer select-none items-center">
         <div className="flex h-8 items-center justify-center rounded-md bg-transparent shadow-card">
           <span
-            className={`flex h-6 w-6 items-center justify-center rounded text-sm ${
-              !isChecked ? "bg-primary-500 text-white" : "text-body-color"
+            className={`flex size-6 items-center justify-center rounded text-sm ${
+              !isChecked ? "bg-primary-500 text-white" : "text-black"
             }`}
           >
             <p>TH</p>
           </span>
           <span
-            className={`flex h-6 w-6 items-center justify-center rounded text-sm ${
-              isChecked ? "bg-primary-500 text-white" : "text-body-color"
+            className={`flex size-6 items-center justify-center rounded text-sm ${
+              isChecked ? "bg-primary-500 text-white" : "text-black"
             }`}
           >
             <p>EN</p>
