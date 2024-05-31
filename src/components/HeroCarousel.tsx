87b49b1aea8caefc657a8b9a8113ef49754daf75
boomchanotai <boom9387@hotmail.com>
2024-05-31 19:37:09 +0700

    fix: hero

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -14,10 +14,13 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { useTranslations } from "@/i18n/utils";
+import type { lang } from "@/types";
 
 const slides = [heroImg1, heroImg2, heroImg3, heroImg4, heroImg5, heroImg6];
 
-export default function carousel() {
+export default function carousel({ lang }: { lang: lang }) {
+  const t = useTranslations(lang);
   const plugin = React.useRef(
     Autoplay({ delay: 3000, stopOnInteraction: true })
   );
@@ -27,22 +30,22 @@ export default function carousel() {
       <CarouselContent>
         {slides.map((slide, index) => (
           <CarouselItem key={index}>
-            <div className="relative z-0">
+            <div className="relative z-0 bg-black">
               <img
                 src={slide.src}
-                className="z-0 flex h-[50vh] min-w-full items-end bg-black bg-cover bg-center bg-no-repeat lg:h-[70vh]"
+                className="z-0 flex h-[50vh] min-w-full items-end object-cover opacity-80 lg:h-[70vh]"
               />
-              <div className="absolute bottom-[10%] z-10 p-[10%]">
+              <div className="absolute bottom-0 z-10 p-[10%]">
                 <h1 className="mb-6 font-Inter text-5xl text-white">
-                  {/* {t("hero.title")} */}TEST
+                  {t("hero.title")}
                 </h1>
                 <p className="max-w-80 text-sm text-white">
-                  {/* {t("hero.description")} */}TEST
+                  {t("hero.description")}
                 </p>
 
                 <a href={`/contact`}>
                   <button className="mt-12 items-center rounded-3xl bg-white p-2 px-4 text-center text-base text-accent-dark-blue transition-colors duration-300 ease-in-out hover:bg-accent-dark-blue hover:text-white">
-                    {/* {t("hero.contact")} */}TEST
+                    {t("hero.contact")}
                   </button>
                 </a>
               </div>
