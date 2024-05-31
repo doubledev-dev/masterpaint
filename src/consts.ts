7bfb48b6055b448e7a9799c7c5f8b8cf083563ee
boomchanotai <boom9387@hotmail.com>
2024-05-31 14:46:46 +0700

    feat: seo

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = "MasterPaint 2548";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "MASTER PAINT 2548 CO.,LTD.";
+export const SITE_DESCRIPTION =
+  "MASTER PAINT 2548 CO.,LTD. รับบริการงานทาสีโดยทีมช่าง ผู้ชำนาญงานเฉพาะด้านและมีประสบการณ์ โดยตรงเชี่ยวชาญด้านงานสีโดยเฉพาะ";
