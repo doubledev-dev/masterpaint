3d2caa10751bd8169558b422f3b602b0ac7354db
ChanatpakornD <csirintronsopon8@gmail.com>
2024-06-01 16:19:17 +0700

    fixed: ProjectCard

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -15,33 +15,27 @@ const ProjectCard = ({
 }: props) => {
   if (title != "" && slugRef != "") {
     return (
-      <a href={`/${lang}/projects/${slugRef}`}>
-        <article className="relative inline-block bg-black duration-300 hover:scale-105">
-          <div>
-            <img
-              className="z-0 block aspect-square h-full object-cover object-center opacity-70"
-              src={thumbnail}
-              alt={title}
-              loading="eager"
-            />
-          </div>
+      <a href={`/${lang}/projects/${slugRef}`} className=" size-full aspect-square relative inline-block bg-black duration-300 hover:scale-105">
+          <img
+            className="z-0 block opacity-70 size-full aspect-square object-cover object-center"
+            src={thumbnail}
+            alt={title}
+            loading="eager"
+          />
           <div className="absolute left-0 top-[10%] z-10 mx-[10%]">
             <h3 className="text-base text-white lg:text-3xl">{title}</h3>
           </div>
-        </article>
       </a>
     );
   } else {
     return (
-      <article className="relative inline-block  duration-300 ">
-        <div>
+      <article className="relative inline-block size-full duration-300 aspect-square ">
           <img
             className="z-0 block aspect-square object-cover object-center"
             src={thumbnail}
             alt="_blank"
             loading="eager"
           />
-        </div>
         <div className="absolute left-0 top-[10%] z-10 mx-[10%]">
           <h3 className="text-base text-white lg:text-3xl">{title}</h3>
         </div>
